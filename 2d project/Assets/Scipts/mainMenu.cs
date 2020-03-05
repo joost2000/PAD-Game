60f3556039c5cfd97db9e447af10215834376ca0
joost2000 <joostottens@hotmail.nl>
2020-03-05 12:42:00 +0100

    idk IM TRYING OKE

---
+++ 2d project/Assets/Scipts/mainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
