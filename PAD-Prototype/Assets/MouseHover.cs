af2594b4fe015a17539f313c64de189e941a56ae
joost2000 <2035831@talnet.nl>
2020-03-06 11:20:03 +0100

    pause menu + changed SampleScene to PlayingScene

--- PAD-Prototype/Assets/MouseHover.cs
+++ PAD-Prototype/Assets/MouseHover.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseHover : MonoBehaviour
 {
-    public;
+    public TMPro.TextMeshProUGUI text;
+    public GameObject playButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Debug.Log(text.fontStyle);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnMouseOver()
     {
-        
+        Debug.Log("testing");
+        text.fontStyle = TMPro.FontStyles.Underline;
     }
 }
