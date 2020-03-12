9af6afc12bdf416dd3929dc871d7d4ef29eb7b87
EeleRoet <eele.roet@hotmail.nl>
2020-03-12 09:48:04 +0100

    assets weer toegevoegd

---
+++ PAD-Prototype/Assets/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject laser;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot(float helling)
+    {
+
+    }
+}
