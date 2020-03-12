fc14d5f868becc47fd8993f571a21e07ce1a8603
EeleRoet <eele.roet@hotmail.nl>
2020-03-12 12:07:15 +0100

    laser lifetime

--- PAD-Prototype/Assets/LaserShoot.cs
+++ PAD-Prototype/Assets/LaserShoot.cs
@@ -6,20 +6,40 @@ public class LaserShoot : MonoBehaviour
 {
     public GameObject laser;
     private Rigidbody2D laserBody;
+
+    private int lifetime;
+    private bool active;
     // Start is called before the first frame update
     void Start()
     {
+        laser.active = false;
         laserBody = laser.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (active)
+        {
+            CheckLifeTime();
+        }
+    }
+
+    private void CheckLifeTime()
+    {
+        lifetime--;
+        if ( lifetime <= 0)
+        {
+            active = false;
+            laser.active = false;
+        }
     }
 
     public void Shoot(float angle)
     {
+        active = true;
+        laser.active = true;
         laserBody.SetRotation(angle);
+        lifetime = 20;
     }
 }
